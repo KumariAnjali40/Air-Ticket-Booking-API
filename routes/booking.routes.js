95ba83917d2054451313e7420ee6483dc3ce9d9e
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-03-11 15:43:36 +0530

    get booking

--- routes/booking.routes.js
+++ routes/booking.routes.js
@@ -5,10 +5,35 @@ const Booking = require('../model/booking.model');
 
 const bookingRouter = express.Router();
 
+//post booking ..
+bookingRouter.post('/api/booking', auth, async (req, res) => {
+    try {
+      const { flightId } = req.body;
+      const booking = new Booking({
+        user: req.body.userID,
+        flight: flightId
+      });
+      await booking.save();
+      res.status(201).send('Booking successful');
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+});
 
 
+//get booking..
+bookingRouter.get('/dashboard', auth, async (req, res) => {
+    try {
+      const bookings = await Booking.find({ user: req.body.userID }).populate('user flight');
+      res.status(200).json(bookings);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+});
 
 
+//
+  
 
 module.exports={
     bookingRouter,
