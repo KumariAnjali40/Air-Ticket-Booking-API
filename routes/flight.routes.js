ed456b3d82adfc213b1a2f7718df19332abea607
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-03-11 15:34:44 +0530

    flight routes done

--- routes/flight.routes.js
+++ routes/flight.routes.js
@@ -17,6 +17,19 @@ flightRouter.get('/api/flights',async(req,res)=>{
 
 
 
+//get flight by id 
+
+flightRouter.get('/:id', async (req, res) => {
+    try {
+      const flight = await Flight.findById(req.params.id);
+      if (!flight) return res.status(404).send('Flight not found');
+      res.status(200).json(flight);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  });
+
+
 //post flight 
 
 flightRouter.post('/api/flight', auth, async (req, res) => {
@@ -40,6 +53,45 @@ flightRouter.post('/api/flight', auth, async (req, res) => {
   });
 
 
+
+  //update the flight 
+
+  flightRouter.put('/api/flight/:id', auth, async (req, res) => {
+    try {
+      const { airline, flightNo, departure, arrival, departureTime, arrivalTime, seats, price } = req.body;
+      const flight = await Flight.findByIdAndUpdate(
+        req.params.id,
+        {
+          airline,
+          flightNo,
+          departure,
+          arrival,
+          departureTime,
+          arrivalTime,
+          seats,
+          price
+        },
+        { new: true }
+      );
+      if (!flight) return res.status(404).send('Flight not found');
+      res.status(204).send('Flight details updated successfully');
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  });
+
+
+
+// delete the flight ..
+flightRouter.delete('/api/flight/:id', auth, async (req, res) => {
+    try {
+      const flight = await Flight.findByIdAndDelete(req.params.id);
+      if (!flight) return res.status(404).send('Flight not found');
+      res.status(202).send('Flight deleted successfully');
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  });
 
 
 module.exports={
